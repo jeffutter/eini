f63e7c0cd90963b6067716a92be8b449d0958221
Jeffery Utter <jeff@jeffutter.com>
2018-03-14 15:50:32 -0500

    Support decrypting

--- cmd/decrypt.go
+++ cmd/decrypt.go
@@ -5,11 +5,83 @@ import (
 
 	"github.com/spf13/cobra"
 
+	"bufio"
+	"encoding/hex"
+	"github.com/Shopify/ejson/crypto"
 	"gopkg.in/yaml.v2"
 	"io/ioutil"
+	"os"
+	"reflect"
+	"regexp"
 )
 
 var output string
+var privKey string
+
+func flatten_yaml(val interface{}) map[string]string {
+	var output = make(map[string]string)
+	switch reflect.TypeOf(val).Kind() {
+	case reflect.Map:
+		var m = reflect.ValueOf(val).Interface().(map[interface{}]interface{})
+		for k, v := range m {
+			var key string
+			switch reflect.TypeOf(k).Kind() {
+			case reflect.String:
+				key = reflect.ValueOf(k).Interface().(string)
+			}
+
+			switch reflect.TypeOf(v).Kind() {
+			case reflect.String:
+				var s = reflect.ValueOf(v).Interface().(string)
+				output[key] = s
+			case reflect.Map:
+				o := flatten_yaml(reflect.ValueOf(v).Interface())
+				for sk, sv := range o {
+					output[key+"_"+sk] = sv
+				}
+			default:
+				fmt.Printf("Unknown Type: %v", reflect.TypeOf(v))
+			}
+		}
+	}
+	return output
+}
+
+func out(data map[string]string) {
+	var priv [32]byte
+	var pub [32]byte
+
+	encryptedRegex, _ := regexp.Compile("^EJ\\[.*\\]")
+	ignoreKeyRegex, _ := regexp.Compile("^_.*")
+
+	pubKey := data["_public_key"]
+
+	pubkey, _ := hex.DecodeString(pubKey)
+	privkey, _ := hex.DecodeString(privKey)
+
+	copy(pub[:], pubkey)
+	copy(priv[:], privkey)
+
+	myKP := crypto.Keypair{
+		Public:  pub,
+		Private: priv,
+	}
+	decrypter := myKP.Decrypter()
+
+	for k, v := range data {
+		if !ignoreKeyRegex.MatchString(k) {
+			if encryptedRegex.MatchString(v) {
+				decrypted, err := decrypter.Decrypt([]byte(v))
+				if err != nil {
+					fmt.Printf("Decryption Error: %v - %v\n", err, v)
+				}
+				fmt.Printf("declare -x \"%s\"=\"%s\"\n", k, decrypted)
+			} else {
+				fmt.Printf("declare -x \"%s\"=\"%s\"\n", k, v)
+			}
+		}
+	}
+}
 
 // decryptCmd represents the decrypt command
 var decryptCmd = &cobra.Command{
@@ -23,30 +95,24 @@ This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Args: cobra.ExactArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Decrypting to: " + output)
+		reader := bufio.NewReader(os.Stdin)
+		privKey, _ = reader.ReadString('\n')
+
 		b, err := ioutil.ReadFile(args[0])
 		if err != nil {
 			fmt.Print(err)
 		}
+
 		var val interface{}
 		err = yaml.Unmarshal(b, &val)
 		if err != nil {
 			fmt.Println("unmarshal []byte to yaml failed: " + err.Error())
 		}
-		fmt.Printf("%v\n", val)
+		out(flatten_yaml(val))
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(decryptCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// decryptCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
 	decryptCmd.Flags().StringVarP(&output, "output", "o", "env", "output format: [env, yaml]")
 }
